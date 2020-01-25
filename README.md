8e6d6a00da71608bb2846075d9796fca18f74a51
redamoon <yuuki60824@gmail.com>
2020-01-25 15:02:08 +0900

    [add] radioボタン

--- README.md
+++ README.md
@@ -20,3 +20,60 @@ $ yarn generate
 ```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+### インストール編
+
+Nuxt.jsの公式ドキュメントに従い、Nuxt.jsの環境を構築します。  
+npm もしくは yarn がインストールされていることが前提になります。  
+※npx は NPM 5.2.0 からデフォルトでバンドルされています。Node8.2以降であればデフォルトで npxが利用できます。  
+npxはローカルにインストールしたnpmパッケージバイナリを直接実行できるコマンドになります。  
+`run-script` の設定せずにローカルインストールしたコマンドを実行することができます。  
+`create-nuxt-app` というコマンドでNuxtの基本的な環境を用意してくれるコマンドのため、`create-nuxt-app`を使いインストールを進めます。
+
+```sh
+npx create-nuxt-app  sacss-netlify-forms-nuxt
+or
+yarn create nuxt-app sacss-netlify-forms-nuxt
+```
+
+```sh
+✨ Generating Nuxt.js project in sacss-netlify-forms-nuxt
+? Project name sacss-netlify-forms-nuxt
+? Project description My praiseworthy Nuxt.js project
+? Author name redamoon
+? Choose the package manager Yarn
+? Choose UI framework Element
+? Choose custom server framework None (Recommended)
+? Choose Nuxt.js modules Axios, DotEnv
+? Choose linting tools ESLint, Prettier, StyleLint
+? Choose test framework None
+? Choose rendering mode Universal (SSR)
+? Choose development tools (Press <space> to select, <a> to toggle all, <i> to invert selection)
+```
+
+```sh
+cd sacss-netlify-forms-nuxt
+yarn dev
+```
+
+#### stylelintのエラー解消
+
+stylelintでエラーがでるので、rulesのkeyを入れておきます。
+
+```js
+module.exports = {
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+}
+↓
+module.exports = {
+  "rules": {}
+}
+```
+
+### Sass install
+
+```sh
+yarn add -D node-sass sass-loader
+```
+
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,19 +1,44 @@
 <template>
   <layout-container>
     <layout-inner>
-      <list-tags tags-name="ul">
-        <li>ほげほげ</li>
-        <li>ほげほげ</li>
-        <li>ほげほげ</li>
-      </list-tags>
       <base-link name="戻る" path="/" />
       <form-container name="sacss">
         <label for="form-name">名前</label>
-        <input id="form-name" name="name" autocomplete="name" type="text" />
+        <input id="form-name" name="name" type="text" />
         <label for="form-email">メールアドレス</label>
-        <input id="form-email" name="email" autocomplete="email" type="email" />
+        <input id="form-email" name="email" type="email" />
         <label for="form-url">URL</label>
-        <input id="form-url" name="url" autocomplete="url" type="url" />
+        <input id="form-url" name="url" type="url" />
+        <p>満足度</p>
+        <list-tags tags-name="ul">
+          <li>
+            <label for="form-good">参加してよかった</label>
+            <input
+              id="form-good"
+              name="form-satisfaction"
+              type="radio"
+              value="楽しかった"
+            />
+          </li>
+          <li>
+            <label for="form-usually">普通</label>
+            <input
+              id="form-usually"
+              name="form-satisfaction"
+              type="radio"
+              value="普通"
+            />
+          </li>
+          <li>
+            <label for="form-not-good">良くなかった</label>
+            <input
+              id="form-not-good"
+              name="form-satisfaction"
+              type="radio"
+              value="楽しくなかった"
+            />
+          </li>
+        </list-tags>
         <button type="submit">送信</button>
       </form-container>
     </layout-inner>
@@ -37,3 +62,9 @@ export default {
   }
 }
 </script>
+
+<style>
+li {
+  text-align: left;
+}
+</style>
